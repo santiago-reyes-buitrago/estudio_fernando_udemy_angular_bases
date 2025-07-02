70468991112134f0025e89c5c6a5f378237ed60b
Santiago Reyes <santiago.reyes@platinum.net.co>
2025-07-02 08:20:33 -0500

    finalizando seccion 5

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,12 +1,15 @@
 import {effect, Injectable, signal} from '@angular/core';
 import type {Character} from '../interface/character.interface';
 
+const loadFromLocalStorage = ():Character[] => {
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters): []
+}
+
 @Injectable({providedIn: 'root'})
 export class DragonballSuperService {
-  characters = signal<Character[]>([
-    {id: 1, name: 'Goku', power: 9001},
-    {id: 2, name: 'Vegeta', power: 8000},
-  ]);
+  characters = signal<Character[]>(loadFromLocalStorage());
+
 
   saveToLocalStorage =  effect(()=>{
     // console.log(`Character count: ${this.characters().length}`);
