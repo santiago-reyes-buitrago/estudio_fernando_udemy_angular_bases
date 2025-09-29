ee0581eb7aa9fa83c2b32afcfe7531fd43085826
Santiago Reyes <santiago.reyes@platinum.net.co>
2025-09-29 09:24:02 -0500

    Repasando secciones faltantes dejadas al comienzo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    //Hash Strategies
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    }
   ]
 };
